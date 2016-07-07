8fc98c0d23d4ebab9c1a26c848171e0b9872d991
williamjohngardner <bill@williamjohngardner.com>
2016-07-07 11:51:06 -0400

    Added index template

--- hello_heroku/urls.py
+++ hello_heroku/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-# from app.views import IndexView
+from app.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^$', IndexView.as_view(), name="index_view")
+    url(r'^$', IndexView.as_view(), name="index_view")
 ]
